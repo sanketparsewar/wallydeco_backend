44f7a250ec16ca3426b269600300e399633e1eaa
sanketp2001 <sanket.p@angularminds.in>
2025-04-18 14:59:36 +0530

    updates

--- src/routes/wallpaper.routes.js
+++ src/routes/wallpaper.routes.js
@@ -11,18 +11,16 @@ const {
   getWallpapersByCategory,
 } = require("../controllers/wallpaper.controller");
 const validate = require("../middlewares/validateMiddleware");
-const {
-  WallpaperValidation,
-} = require("../utils/wallpaperValidation");
+const { WallpaperValidation, } = require("../utils/wallpaperValidation");
 const { verifyJwt } = require("../middlewares/auth.middleware");
 
-router.post("/", verifyJwt, validate(WallpaperValidation), addWallpaper);
-router.put("/:id", verifyJwt, validate(WallpaperValidation), updateWallpaper); // Update a wallpaper
-router.get("/favourite", verifyJwt, getFavouriteWallpapers);
+router.get("/", getWallpapers);
+router.get("/favourites", verifyJwt, getFavouriteWallpapers);
+router.get("/category/:category", getWallpapersByCategory);
 router.get("/favourite/:id", verifyJwt, addWallpaperToFavourite);
 router.get("/:id", getWallpaperById);
-router.get("/", getWallpapers);
-router.get("/category/:category", getWallpapersByCategory);
+router.post("/", verifyJwt, validate(WallpaperValidation), addWallpaper);
+router.put("/:id", verifyJwt, validate(WallpaperValidation), updateWallpaper);
 router.delete("/:id", verifyJwt, deleteWallpaper);
 
 module.exports = router;
