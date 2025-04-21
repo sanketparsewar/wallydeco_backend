0210df1d429bd6938b847b482987846bceea264b
sanketp2001 <sanket.p@angularminds.in>
2025-04-21 18:21:59 +0530

    added middleware to get user orders

--- src/routes/order.routes.js
+++ src/routes/order.routes.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const router = express.Router();
-const { placeOrder, getAllOrders, getOrderById, updateOrderStatus, deleteOrder } = require("../controllers/order.controller");
+const { placeOrder,getUserOrders, getAllOrders, getOrderById, updateOrderStatus, deleteOrder } = require("../controllers/order.controller");
 const { verifyJwt } = require("../middlewares/auth.middleware");
 
 router.post("/", verifyJwt, placeOrder);
+router.get("/user",verifyJwt, getUserOrders);
 router.get("/", getAllOrders);
 router.get("/:id", getOrderById);
 router.put("/:id", updateOrderStatus);
