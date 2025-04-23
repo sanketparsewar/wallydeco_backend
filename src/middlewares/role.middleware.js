0fb32a0ee37b89f35cda3942c2eba336ded03d2c
sanketp2001 <sanket.p@angularminds.in>
2025-04-23 15:39:24 +0530

    role checking middleware for admin

---
+++ src/middlewares/role.middleware.js
@@ -0,0 +1,46 @@
+const jwt = require("jsonwebtoken");
+const User = require("../models/user")
+
+
+exports.isAdmin = async (req, res, next) => {
+    try {
+        const user = req.user
+        if (user.role !== "admin") {
+            return res.status(403).json({ message: "Unauthorized access" })
+        }
+        next()
+    } catch (error) {
+        res.status(401).json({ message: error.message })
+    }
+}
+
+
+// exports.verifyJwt=async(req,res,next)=>{
+//     try {
+//         const token=req.header("Authorization").replace("Bearer ","")
+//         if(!token){
+//             return res.status(400).json({message:"Token is not provided"})
+//         }
+//         const decode=jwt.verify(token,process.env.ACCESS_TOKEN_SECRET_KEY)
+//         const user=await User.findById(decode?._id).select("-password")
+//         if(!user){
+//             return res.status(403).json({message:"Unauthorized access"})
+//         }
+//         req.user=user
+//         next()
+//     }
+//     catch (error) {
+//         res.status(401).json({message:error.message})
+//     }
+// }
+exports.isUser = async (req, res, next) => {
+    try {
+        const user = req.user
+        if (user.role !== "user") {
+            return res.status(403).json({ message: "Unauthorized access" })
+        }
+        next()
+    } catch (error) {
+        res.status(401).json({ message: error.message })
+    }
+}
