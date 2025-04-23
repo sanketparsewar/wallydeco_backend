f2eabdb7a78e43354b70fd76f0107e9b309299b1
sanketp2001 <sanket.p@angularminds.in>
2025-04-23 15:38:18 +0530

    added new routes to get all orders

--- src/routes/order.routes.js
+++ src/routes/order.routes.js
@@ -1,14 +1,15 @@
 const express = require("express");
 const router = express.Router();
-const { placeOrder,getUserOrders, getAllOrders, getOrderById,cancelOrder,markAsDelivered, updateOrderStatus, deleteOrder } = require("../controllers/order.controller");
+const { placeOrder, getUserOrders, getAllOrders, getOrderById, cancelOrder, markAsDelivered, updateOrderStatus, deleteOrder } = require("../controllers/order.controller");
 const { verifyJwt } = require("../middlewares/auth.middleware");
+const { isAdmin } = require("../middlewares/role.middleware");
 
 router.post("/", verifyJwt, placeOrder);
-router.get("/user",verifyJwt, getUserOrders);
-router.get("/", getAllOrders);
+router.get("/user", verifyJwt, getUserOrders);
+router.get("/", verifyJwt, isAdmin, getAllOrders);
 router.get("/:id", getOrderById);
 router.put('/:orderId/cancel', cancelOrder);
-router.put('/:orderId/delivered',markAsDelivered);
+router.put('/:orderId/update', verifyJwt, isAdmin, updateOrderStatus);
 router.put("/:id", updateOrderStatus);
 router.delete("/:id", deleteOrder);
 
